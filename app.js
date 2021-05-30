ca7614ef65ddc33d33f924352d2cbe7813e5eefb
Serlin Tamilselvam <t.serlin94@gmail.com>
2021-05-30 18:01:12 -0400

    Added temp website url

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ const PORT = process.env.PORT
 const app = express()
 
 const corsOptions = {
-    origin: [process.env.ALLOWED_FRONT_END_URL_1, process.env.ALLOWED_FRONT_END_URL_2, process.env.ALLOWED_FRONT_END_URL_3],
+    origin: [process.env.ALLOWED_FRONT_END_URL_1, process.env.ALLOWED_FRONT_END_URL_2, 
+        process.env.ALLOWED_FRONT_END_URL_3, process.env.ALLOWED_FRONT_END_URL_4],
     optionsSuccessStatus: 200
 }
 
